e011a9259598b3e5ff03f573497511e446eccfa4
Jan-Jaap Driessen <devnull@localhost>
2010-12-02 16:50:32 +0100

    Also need the paster configuration parsing in the Fanstatic component.

--- fanstatic/wsgi.py
+++ fanstatic/wsgi.py
@@ -1,3 +1,5 @@
+from paste.util.converters import asbool
+
 import fanstatic
 
 def Fanstatic(app, **config):
@@ -8,4 +10,13 @@ def Fanstatic(app, **config):
     return fanstatic.Delegator(inject, publisher_signature=signature)
 
 def make_fanstatic(app, global_config, **local_config):
+    devmode = local_config.get('devmode')
+    if devmode is not None:
+        local_config['devmode'] = asbool(devmode)
+    rollup = local_config.get('rollup')
+    if rollup is not None:
+        local_config['rollup'] = asbool(rollup)
+    bottom = local_config.get('bottom')
+    if bottom is not None:
+        local_config['bottom'] = asbool(bottom)
     return Fanstatic(app, **local_config)
