bf1ef84bd948cd478db3cc2b747887a9b9092ed9
Jan-Jaap Driessen <devnull@localhost>
2011-01-18 15:48:08 +0100

    The next release will be 0.10. Update the changelog.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
 
 setup(
     name='fanstatic',
-    version='0.9adev',
+    version='0.10dev',
     description="Flexible static resources for web applications.",
     classifiers=[],
     keywords='',
