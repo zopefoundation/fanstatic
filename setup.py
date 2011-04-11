bebb111bcfe6973b181f71a952c76e39c517767d
Jan-Jaap Driessen <devnull@localhost>
2011-04-11 19:41:55 +0200

    Preparing release 0.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
 
 setup(
     name='fanstatic',
-    version='0.11dev',
+    version='0.11',
     description="Flexible static resources for web applications.",
     classifiers=[],
     keywords='',
