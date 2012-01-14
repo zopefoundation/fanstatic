1e6715f0f5d5af49419d0aa8d11970f6551fca29
Martijn Faassen <faassen@#startifact.com>
2012-01-14 17:51:35 +0100

    Preparing release 0.11.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
 
 setup(
     name='fanstatic',
-    version='0.11.4dev',
+    version='0.11.4',
     description="Flexible static resources for web applications.",
     classifiers=[],
     keywords='',
