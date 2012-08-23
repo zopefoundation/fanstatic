2f13c120d06833e1d8d7d6a728b1f2188bbacb5c
Jan-Jaap Driessen <devnull@localhost>
2012-08-23 22:30:53 +0200

    Back to development: 0.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
 
 setup(
     name='fanstatic',
-    version='0.13.2',
+    version='0.14dev',
     description="Flexible static resources for web applications.",
     classifiers=[],
     keywords='',
