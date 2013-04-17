00af04156d3efa665fc56f9e792dd8b62dfc62f1
Jan-Jaap Driessen <devnull@localhost>
2013-04-18 00:34:58 +0200

    Preparing release 1.0a

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(Command):
 
 setup(
     name='fanstatic',
-    version='0.17dev',
+    version='1.0a',
     description="Flexible static resources for web applications",
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
