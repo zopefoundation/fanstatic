50d8e9c56238fab471ea62baa17df8d6bcfdd54f
Jan-Jaap Driessen <devnull@localhost>
2013-09-24 16:21:06 +0200

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PyTest(Command):
 
 setup(
     name='fanstatic',
-    version='1.0a4',
+    version='1.0a5.dev0',
     description="Flexible static resources for web applications",
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
