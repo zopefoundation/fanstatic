05cecc5348edf1f084fa6f348426ecfc27f76215
Michael Howitz <icemac@gmx.net>
2025-03-05 09:21:34 +0100

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 
 setup(
     name='fanstatic',
-    version='1.5',
+    version='1.6.dev0',
     description="Flexible static resources for web applications",
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
